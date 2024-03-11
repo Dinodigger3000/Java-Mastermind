7d6fdfb2c11a401f944bebb6425d155457cd1c76
Makol J.Chuol <jchuol@macalester.edu>
2024-03-10 22:57:05 -0500

    created abstract code

---
+++ src/mastermind/Code.java
@@ -0,0 +1,19 @@
+package mastermind;
+
+import java.util.List;
+
+public abstract class Code {
+
+    private List<GamePiece> code;
+
+    public Code() {
+
+    }
+
+    @Override
+    public String toString() {
+        return code.get(0).name + ", " + code.get(1).name + ", " + code.get(2).name + ", " + code.get(3).name;
+
+    }
+}
+
