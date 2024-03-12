7f4cb4aad60521c7b4a192e4cba5b56bb5c242b9
Makol J.Chuol <jchuol@macalester.edu>
2024-03-11 21:50:43 -0500

    secret code

---
+++ src/mastermind/SecretCode.java
@@ -0,0 +1,18 @@
+package mastermind;
+
+import java.util.Random;
+
+public class SecretCode extends Code{
+    public SecretCode(){
+        super();
+        generateRandomCode(4);
+    }
+    private void generateRandomCode(int length) {
+
+        Random random = new Random();  
+        for (int i = 0; i <length; i++) {
+            code.set(i, GamePiece.values()[random.nextInt(0,5)]);      
+        }
+    }
+
+}
